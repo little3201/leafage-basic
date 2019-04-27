d97af5cbebf7014d1053fed77195d555564793cc
李文强 <liwenqiang@tseverday.com>
2019-04-27 18:10:28 +0800

    vo使用公共的

--- abeille-basic-profile/src/main/java/top/abeille/basic/profile/api/UserApi.java
+++ abeille-basic-profile/src/main/java/top/abeille/basic/profile/api/UserApi.java
@@ -1,9 +1,9 @@
-package top.abeille.basic.profile.api.service;
+package top.abeille.basic.profile.api;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import top.abeille.basic.profile.api.vo.UserInfoVO;
+import top.abeille.common.vo.UserInfoVO;
 
 /**
  * 用户api

2722f669d5965068ae031707049e572a847f6d10
李文强 <liwenqiang@tseverday.com>
2019-05-04 18:49:25 +0800

    修改security

--- abeille-basic-authority/src/main/java/top/abeille/basic/authority/config/security/oauth2/AuthorizationServerConfig.java
+++ abeille-basic-authority/src/main/java/top/abeille/basic/authority/config/security/oauth2/AuthorizationServerConfig.java
@@ -1,6 +1,5 @@
 package top.abeille.basic.authority.config.security.oauth2;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;

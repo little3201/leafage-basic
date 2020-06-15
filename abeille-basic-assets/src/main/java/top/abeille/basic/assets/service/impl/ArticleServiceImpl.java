96f5d43daab2e3d8b68e3e8b0d4cd08539a704bd
李文强 <liwenqiang@tseverday.com>
2020-06-15 17:49:24 +0800

    user拦截配置，article作者配置

--- abeille-basic-assets/src/main/java/top/abeille/basic/assets/service/impl/ArticleServiceImpl.java
+++ abeille-basic-assets/src/main/java/top/abeille/basic/assets/service/impl/ArticleServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import top.abeille.basic.assets.api.HypervisorApi;
+import top.abeille.basic.assets.api.bo.UserBO;
 import top.abeille.basic.assets.constant.PrefixEnum;
 import top.abeille.basic.assets.document.ArticleInfo;
 import top.abeille.basic.assets.document.DetailsInfo;
@@ -135,9 +136,8 @@ public class ArticleServiceImpl extends AbstractBasicService implements ArticleS
     private ArticleVO convertOuter(ArticleInfo info) {
         ArticleVO outer = new ArticleVO();
         BeanUtils.copyProperties(info, outer);
-        hypervisorApi.fetchUserByBusinessId(info.getModifier())
-                .switchIfEmpty(Mono.error(() -> new RuntimeException("调用服务发生异常")))
-                .subscribe(outer::setAuthor);
+        UserBO userBO = hypervisorApi.fetchUserByBusinessId(info.getModifier()).block();
+        outer.setAuthor(userBO);
         return outer;
     }
 

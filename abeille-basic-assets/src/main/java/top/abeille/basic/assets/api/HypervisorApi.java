d74cd7ffff4435867546ac84c167e295141322a9
李文强 <liwenqiang@tseverday.com>
2020-09-15 10:58:55 +0800

    添加hypervisorApi实现

--- abeille-basic-assets/src/main/java/top/abeille/basic/assets/api/HypervisorApi.java
+++ abeille-basic-assets/src/main/java/top/abeille/basic/assets/api/HypervisorApi.java
@@ -3,9 +3,6 @@
  */
 package top.abeille.basic.assets.api;
 
-import org.apache.http.util.Asserts;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import top.abeille.basic.assets.bo.UserBO;
 
@@ -22,10 +19,5 @@ public interface HypervisorApi {
      * @param businessId 业务id
      * @return 如果查询到数据，返回查询到的信息，否则返回404状态码
      */
-    default Mono<UserBO> fetchUserByBusinessId(String businessId) {
-        Asserts.notBlank(businessId, "businessId");
-        return WebClient.create("http://abeille-basic-hypervisor").get().uri("/user/{businessId}", businessId)
-                .accept(MediaType.APPLICATION_JSON)
-                .retrieve().bodyToMono(UserBO.class);
-    }
+    Mono<UserBO> fetchUserByBusinessId(String businessId);
 }

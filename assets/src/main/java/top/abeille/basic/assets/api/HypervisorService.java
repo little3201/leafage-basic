5aabc5467944ce9d44c4241b9a9a5594503d4f96
little3201 <little3201@163.com>
2020-11-25 14:11:52 +0800

    修改服务名

--- assets/src/main/java/top/abeille/basic/assets/api/HypervisorService.java
+++ assets/src/main/java/top/abeille/basic/assets/api/HypervisorService.java
@@ -18,7 +18,7 @@ public class HypervisorService implements HypervisorApi {
     private final WebClient.Builder clientBuilder;
 
     public HypervisorService(WebClient.Builder clientBuilder) {
-        clientBuilder.baseUrl("http://abeille-basic-hypervisor").build();
+        clientBuilder.baseUrl("http://hypervisor").build();
         this.clientBuilder = clientBuilder;
     }
 

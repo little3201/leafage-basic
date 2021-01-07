18f3f82114a9e0e38598eb2ef53e38079830f8ef
little3201 <little3201@163.com>
2021-01-07 10:07:21 +0800

    去掉时间格式化

--- hypervisor/src/main/java/top/abeille/basic/hypervisor/vo/BaseVO.java
+++ hypervisor/src/main/java/top/abeille/basic/hypervisor/vo/BaseVO.java
@@ -1,7 +1,5 @@
 package top.abeille.basic.hypervisor.vo;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
@@ -17,7 +15,6 @@ public class BaseVO implements Serializable {
     /**
      * 修改时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime modifyTime;
 
     public String getCode() {

bded895f126e8277e920a78f693eb586b43042d7
liwenqiang <little3201@163.com>
2021-01-13 23:17:29 +0800

    修改Mongo自动填充注解

--- assets/src/main/java/top/abeille/basic/assets/AssetsApplication.java
+++ assets/src/main/java/top/abeille/basic/assets/AssetsApplication.java
@@ -5,12 +5,12 @@ package top.abeille.basic.assets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.config.EnableReactiveMongoAuditing;
 
 /**
  * @author liwenqiang
  */
-@EnableMongoAuditing
+@EnableReactiveMongoAuditing
 @SpringBootApplication
 public class AssetsApplication {
 

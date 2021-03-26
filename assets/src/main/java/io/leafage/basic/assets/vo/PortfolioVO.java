17a1058b490573543a860a749d7bfffe96e41c09
little3201 <little3201@163.com>
2021-03-26 17:51:30 +0800

    edit url type

--- assets/src/main/java/io/leafage/basic/assets/vo/PortfolioVO.java
+++ assets/src/main/java/io/leafage/basic/assets/vo/PortfolioVO.java
@@ -72,11 +72,11 @@ public class PortfolioVO extends BaseVO {
         this.title = title;
     }
 
-    public String getUrl() {
+    public Set<String> getUrl() {
         return url;
     }
 
-    public void setUrl(String url) {
+    public void setUrl(Set<String> url) {
         this.url = url;
     }
 

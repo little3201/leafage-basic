b8774ff3a808df9a8bde0b8179b665949ad64cf4
liwenqiang <little3201@163.com>
2022-02-09 23:13:08 +0800

    添加unlock接口

--- hypervisor/src/main/java/io/leafage/basic/hypervisor/service/AccountService.java
+++ hypervisor/src/main/java/io/leafage/basic/hypervisor/service/AccountService.java
@@ -5,6 +5,7 @@ package io.leafage.basic.hypervisor.service;
 
 import io.leafage.basic.hypervisor.dto.AccountDTO;
 import io.leafage.basic.hypervisor.vo.AccountVO;
+import reactor.core.publisher.Mono;
 import top.leafage.common.reactive.ReactiveBasicService;
 
 /**
@@ -13,4 +14,12 @@ import top.leafage.common.reactive.ReactiveBasicService;
  * @author liwenqiang 2018/12/17 19:26
  **/
 public interface AccountService extends ReactiveBasicService<AccountDTO, AccountVO, String> {
+
+    /**
+     * unlock account
+     *
+     * @param username 账号
+     * @return 结果
+     */
+    Mono<Boolean> unlock(String username);
 }

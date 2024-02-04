2f1b7153686c7e915b4ccc235825e2a3d85e763f
wq <little3201@163.com>
2024-02-04 20:35:34 +0800

    添加config包

--- assets/src/main/java/io/leafage/basic/assets/config/JpaAuditingConfiguration.java
+++ assets/src/main/java/io/leafage/basic/assets/config/JpaAuditingConfiguration.java
@@ -1,12 +1,13 @@
-package io.leafage.basic.hypervisor.audit;
+package io.leafage.basic.assets.config;
 
+import io.leafage.basic.assets.audit.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
- * jpa 自动填充配置
+ * jpa config.
  *
  * @author liwenqiang  2020-12-20 9:54
  */
--- hypervisor/src/main/java/io/leafage/basic/hypervisor/config/JpaAuditingConfiguration.java
+++ hypervisor/src/main/java/io/leafage/basic/hypervisor/config/JpaAuditingConfiguration.java
@@ -1,12 +1,13 @@
-package io.leafage.basic.assets.audit;
+package io.leafage.basic.hypervisor.config;
 
+import io.leafage.basic.hypervisor.audit.AuditorAwareImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
- * jpa config.
+ * jpa 自动填充配置
  *
  * @author liwenqiang  2020-12-20 9:54
  */

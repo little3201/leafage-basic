7682314c467466a05ed86732c21f71276bf36d60
wq li <little3201@163.com>
2024-10-18 10:20:33 +0800

    配置auth

---
+++ auth/src/main/java/io/leafage/auth/endpoint/TokenController.java
@@ -0,0 +1,47 @@
+package io.leafage.auth.endpoint;
+
+import io.leafage.auth.jose.Jwks;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+/**
+ * A controller for the token resource.
+ *
+ * @author wq li
+ */
+@RestController
+public class TokenController {
+
+    @PostMapping("/token")
+    public String token(Authentication authentication) {
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        // @formatter:off
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+        // @formatter:on
+        return this.jwtEncoder().encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(Jwks.jwkSource());
+    }
+
+}

cbf6f2b04731b8a47c4063041cc4292bc21a9035
wq li <little3201@163.com>
2026-02-04 08:37:03 +0800

    feat: 实现script配置逻辑

--- hypervisor/src/main/java/top/leafage/hypervisor/constants/FieldEnum.java
+++ hypervisor/src/main/java/top/leafage/hypervisor/constants/FieldEnum.java
@@ -15,14 +15,15 @@
 
 package top.leafage.hypervisor.constants;
 
-import static top.leafage.hypervisor.constants.FieldConstant.*;
+
+import static top.leafage.hypervisor.constants.FieldEnum.Type.*;
 
 /**
  * 数据库类型映射到Java类型、组件名和TypeScript类型
  *
  * @author wq li
  */
-public enum FieldTypeEnum {
+public enum FieldEnum {
 
     INT2("int2", JAVA_SHORT, FORM_INPUT_NUMBER, TYPE_NUMBER),
     INT4("int4", JAVA_INTEGER, FORM_INPUT_NUMBER, TYPE_NUMBER),
@@ -33,7 +34,7 @@ public enum FieldTypeEnum {
     BOOL("bool", JAVA_BOOLEAN, FORM_SWITCH, TYPE_BOOLEAN),
     VARCHAR("varchar", JAVA_STRING, FORM_INPUT, TYPE_STRING),
     INET("inet", JAVA_INETADDRESS, FORM_INPUT, TYPE_STRING),
-    TEXT("text", JAVA_STRING, FORM_INPUT, TYPE_STRING),
+    TEXT("text", JAVA_STRING, FORM_TEXTAREA, TYPE_STRING),
     DATE("date", JAVA_LOCALDATE, FORM_DATE_PICKER, TYPE_DATE),
     TIME("time", JAVA_LOCALTIME, FORM_TIME_PICKER, TYPE_DATE),
     TIMESTAMP("timestamp", JAVA_LOCALDATETIME, FORM_DATE_PICKER, TYPE_DATE),
@@ -45,15 +46,15 @@ public enum FieldTypeEnum {
     private final String formType;
     private final String tsType;
 
-    FieldTypeEnum(String dbType, String javaType, String formType, String tsType) {
+    FieldEnum(String dbType, String javaType, String formType, String tsType) {
         this.dbType = dbType;
         this.javaType = javaType;
         this.formType = formType;
         this.tsType = tsType;
     }
 
-    public static FieldTypeEnum fromDbType(String dbType) {
-        for (FieldTypeEnum mapping : values()) {
+    public static FieldEnum fromDbType(String dbType) {
+        for (FieldEnum mapping : values()) {
             if (mapping.dbType.equalsIgnoreCase(dbType)) {
                 return mapping;
             }
@@ -71,6 +72,40 @@ public enum FieldTypeEnum {
 
     public String getTsType() {
         return tsType;
+    }
+
+    public static final class Type {
+        public static final String FORM_INPUT = "input";
+        public static final String FORM_INPUT_NUMBER = "input-number";
+        public static final String FORM_TEXTAREA = "textarea";
+        public static final String FORM_SELECT = "select";
+        public static final String FORM_SWITCH = "switch";
+        public static final String FORM_DATE_PICKER = "date-picker";
+        public static final String FORM_TIME_PICKER = "time-picker";
+
+        public static final String TYPE_NUMBER = "number";
+        public static final String TYPE_STRING = "string";
+        public static final String TYPE_BOOLEAN = "boolean";
+        public static final String TYPE_DATE = "Date";
+
+        public static final String JAVA_SHORT = "Short";
+        public static final String JAVA_INTEGER = "Integer";
+        public static final String JAVA_LONG = "Long";
+        public static final String JAVA_FLOAT = "Float";
+        public static final String JAVA_DOUBLE = "Double";
+        public static final String JAVA_BIGDECIMAL = "BigDecimal";
+        public static final String JAVA_STRING = "String";
+        public static final String JAVA_BOOLEAN = "Boolean";
+        public static final String JAVA_INETADDRESS = "InetAddress";
+        public static final String JAVA_LOCALDATE = "LocalDate";
+        public static final String JAVA_LOCALTIME = "LocalTime";
+        public static final String JAVA_LOCALDATETIME = "LocalDateTime";
+
+        // private construce
+        private Type() {
+            throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+        }
+
     }
 }
 

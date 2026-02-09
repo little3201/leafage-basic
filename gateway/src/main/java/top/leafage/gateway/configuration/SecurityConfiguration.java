3c13b4ee81a4f24c9be32583a8b55fdc4b143d91
wq li <little3201@163.com>
2026-02-09 18:01:05 +0800

    feat: 重名名client-name

--- gateway/src/main/java/top/leafage/gateway/configuration/SecurityConfiguration.java
+++ gateway/src/main/java/top/leafage/gateway/configuration/SecurityConfiguration.java
@@ -72,7 +72,7 @@ public class SecurityConfiguration {
 
     private AuthenticationEntryPoint authenticationEntryPoint() {
         AuthenticationEntryPoint authenticationEntryPoint =
-                new LoginUrlAuthenticationEntryPoint("/oauth2/authorization/web-client-oidc");
+                new LoginUrlAuthenticationEntryPoint("/oauth2/authorization/web-client");
 
         MediaTypeRequestMatcher textHtmlMatcher = new MediaTypeRequestMatcher(MediaType.TEXT_HTML);
         textHtmlMatcher.setUseEquals(true);

56062eea2e702cb96e6a1729395c44c05054a4d9
wq li <little3201@163.com>
2026-02-12 09:57:58 +0800

    feat: 格式化默认client的数据执行语句

--- auth/src/main/resources/schema.sql
+++ auth/src/main/resources/schema.sql
@@ -175,7 +175,17 @@ COMMENT
 COMMENT
     ON TABLE "public"."oauth2_registered_client" IS 'client 表';
 
-INSERT INTO "public"."oauth2_registered_client" ("id", "client_id", "client_id_issued_at", "client_secret", "client_secret_expires_at", "client_name", "client_authentication_methods", "authorization_grant_types", "redirect_uris", "post_logout_redirect_uris", "scopes", "client_settings", "token_settings") VALUES ('c4e1f359-6b10-47a7-bff3-2e952eb0c04e', 'web-client', '2025-02-08 11:41:08', '{bcrypt}$2a$10$ZIakhpE/4Unx0lpsUWu3fejops1oD8VQF1AMnEeKtiOGitN7n7Dh6', NULL, 'web-client', 'client_secret_basic', 'authorization_code', 'http://127.0.0.1:8760/login/oauth2/code/web-client', 'http://127.0.0.1:8760', 'openid,profile,offline_access', '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}', '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}');
+INSERT INTO "public"."oauth2_registered_client" ("id", "client_id", "client_id_issued_at", "client_secret",
+                                                 "client_secret_expires_at", "client_name",
+                                                 "client_authentication_methods", "authorization_grant_types",
+                                                 "redirect_uris", "post_logout_redirect_uris", "scopes",
+                                                 "client_settings", "token_settings")
+VALUES ('c4e1f359-6b10-47a7-bff3-2e952eb0c04e', 'web-client', '2025-02-08 11:41:08',
+        '{bcrypt}$2a$10$ZIakhpE/4Unx0lpsUWu3fejops1oD8VQF1AMnEeKtiOGitN7n7Dh6', NULL, 'web-client',
+        'client_secret_basic', 'authorization_code', 'http://127.0.0.1:8760/login/oauth2/code/web-client',
+        'http://127.0.0.1:8760', 'openid,profile,offline_access',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":false,"settings.client.require-authorization-consent":false}',
+        '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":true,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}');
 
 -- ----------------------------
 -- Table structure for persistent_logins
